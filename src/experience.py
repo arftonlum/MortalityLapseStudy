99895a6a3eb0b8bdf80d6e5743f83b5689198e6d
Nate <nate.brueck@gmail.com>
2026-07-07 10:16:08 -0600

    added a mortality multiplier to simulate real-world assumptions

--- src/experience.py
+++ src/experience.py
@@ -1,14 +1,49 @@
 import numpy as np
 import pandas as pd
 
+def assign_mortality_multiplier(portfolio, young = .9, middle = 1, old = 1.15):
+    multiplier = np.ones(len(portfolio))
+
+    multiplier *= np.where(
+        portfolio["attained_age"] < 50,
+        young,
+        np.where(
+            portfolio["attained_age"] >= 75,old,1.00,
+        )
+    )
+    multiplier *= np.where(
+        portfolio["policy_duration"] <= 5,
+        0.85,
+        middle,
+    )
+    noise = np.random.normal(
+        loc=1.0,
+        scale=0.05,
+        size=len(portfolio)
+    )
+
+    multiplier *= noise
+
+    multiplier = np.clip(
+        multiplier,
+        0.70,
+        1.30
+    )
+    
+
 def simulate_deaths(portfolio: pd.DataFrame, seed: int = 42) -> pd.DataFrame:
     portfolio = portfolio.copy()
 
     np.random.seed(seed)
 
+    portfolio["actual_qx"] = (
+        portfolio["qx"] *
+        portfolio["mortality_multiplier"]
+    )
+
     portfolio["death"] = np.random.binomial(
         n=1,
-        p=portfolio["qx"]
+        p=portfolio["qx"] * multiplier
     )
 
     portfolio["expected_deaths"] = portfolio["qx"]
@@ -43,3 +78,41 @@ def summarize_experience(portfolio: pd.DataFrame) -> pd.DataFrame:
     )
 
     return experience
+
+def summarize_by_age_band(portfolio: pd.DataFrame) -> pd.DataFrame:
+    bins = [20, 30, 40, 50, 60, 70, 80, 90, 101]
+    labels = [
+        "20–29",
+        "30–39",
+        "40–49",
+        "50–59",
+        "60–69",
+        "70–79",
+        "80–89",
+        "90–100"
+    ]
+
+    portfolio["age_band"] = pd.cut(
+        portfolio["attained_age"],
+        bins=bins,
+        labels=labels,
+        right=False
+    )
+
+
+    band_experience = (
+        portfolio
+        .groupby("age_band")
+        .agg(
+            exposure=("policy_id", "count"),
+            expected_deaths=("expected_deaths", "sum"),
+            actual_deaths=("death", "sum")
+        )
+    )
+
+    band_experience["AE"] = (
+        band_experience["actual_deaths"] /
+        band_experience["expected_deaths"]
+    )
+
+    band_experience
